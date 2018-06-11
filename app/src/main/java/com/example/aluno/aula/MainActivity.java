a3564638b6335a1a0147d674dfc355b75568e8aa
Raquel <raquel.dossantos1965@gmail.com>
2018-06-11 08:45:33 -0300

    Criação do projeto.

--- app/src/main/java/com/example/aluno/aula/MainActivity.java
+++ app/src/main/java/com/example/aluno/aula/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.aluno.aula;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+        private Button btnTela2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTela2 = (Button) findViewById(R.id.buttonTela2);
+
+        btnTela2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //O que fazer quando clicar neste botão
+            }
+        });
     }
 }
