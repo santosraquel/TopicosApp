51d4a255dec68baaf2dae460966a2075f053378d
aluno <raquel.dossantos@gmail.com>
2018-06-18 10:09:28 -0300

    conta

--- app/src/main/java/com/example/aluno/aula/Tela3Activity.java
+++ app/src/main/java/com/example/aluno/aula/Tela3Activity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Tela3Activity extends AppCompatActivity {
     private EditText EditTextValor;
     private EditText EditTextCotacao;
     private Button buttonCalcular;
+    private TextView conversao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,7 @@ public class Tela3Activity extends AppCompatActivity {
         EditTextValor = (EditText) findViewById(R.id.editTextValor);
         EditTextCotacao = (EditText) findViewById(R.id.editTextCotacao);
         buttonCalcular = (Button) findViewById(R.id.buttonCalcular);
+        conversao = (TextView) findViewById(R.id.textConversao);
 
         buttonCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,8 +28,8 @@ public class Tela3Activity extends AppCompatActivity {
                 double valor = Double.parseDouble(EditTextValor.getText().toString());
                 double valorCot = Double.parseDouble(EditTextCotacao.getText().toString());
 
-                double Resultado = (valor*valorCot);
-
+                double ValorReal = (valor*valorCot);
+                conversao.setText("O valor em dólar é:"+ValorReal);
 
 
             }
